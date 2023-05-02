4afb2967cc5a3f9a3411b705c299c00f4ca9c9d1
diegogqcr <diego.gamboa.cr@gmail.com>
2023-05-02 01:30:40 -0600

    feat: add image gambo.png and h1

--- components/header.js
+++ components/header.js
@@ -4,10 +4,10 @@ function Header() {
     return (
         //alinear elementos en el centro del header y en linea del box 
         <div className="flex items-center justify-center p-2 bg-gray-800">
-            <Image src="/static/images/logo.png" width={36} height={36} alt="logo de la plataforma"></Image>
+            <Image src="/static/images/GAMBO3.png" width={83} height={73} alt="logo de la plataforma"></Image>
             <div className="flex justify-center m-4 space-x-0 text-center md:text-xl">
-                <h1 className="p-1 font-bold text-red-400">Diego</h1>
-                <h1 className="p-1 font-bold text-blue-400">Gamboa</h1>
+                <h1 className="p-1 font-bold text-red-400">Aplicación de</h1>
+                <h1 className="p-1 font-bold text-blue-400">Tareas</h1>
             </div>
         </div>
     )
